413ceb4be0578f7761ea836666d1b40afe6392e1
Xavi Loinaz <Xavi@xavis-macbook-pro.local>
2016-01-03 01:46:44 +0800

    Initial Commit

---
+++ Psifofor/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Psifofor
+//
+//  Created by Xavi Loinaz on 1/3/16.
+//  Copyright © 2016 Xavi Loinaz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
